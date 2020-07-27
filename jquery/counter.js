1edef6c7df19c47a86fc61019346f53479abd867
feryal abdullah <feryalabdullah26@gmail.com>
2020-07-27 15:57:16 +0300

    Add the code for count the characters

--- jquery/counter.js
+++ jquery/counter.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
 
+    var mMaxLength = 200;
+
+    $(".remaining").text(mMaxLength);
+
+    $("#post_id").keydown(function () {
+        LimitCharacters($(this));
+    });
+    $("#post_id").keyup(function () {
+        LimitCharacters($(this));
+    });
+
+    function LimitCharacters(post_id) {
+        if (post_id.val().length > mMaxLength) {
+            post_id.val(post_id.val().substring(0, mMaxLength));
+        } else {
+            var mRemaining = mMaxLength - post_id.val().length;
+            $(".remaining").text(mRemaining);
+        }
+    }
 });
